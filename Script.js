3bd0cff37f754a4bd87bb537b33d4db60b3be4c9
Morena131 <anamorenareis63@gmail.com>
2024-10-01 15:27:33 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,15 @@
+document.getElementById("valueForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    const values = [
+        parseInt(document.getElementById("value1").value, 10),
+        parseInt(document.getElementById("value2").value, 10),
+        parseInt(document.getElementById("value3").value, 10),
+        parseInt(document.getElementById("value4").value, 10),
+        parseInt(document.getElementById("value5").value, 10)
+    ];
+
+    const randomIndex = Math.floor(Math.random() * values.lenght);
+    const randomValue = values[randomIndex];
+
+    document.getElementById("result").textContent = `Valor Sorteado: ${randomValue}`;
+});
